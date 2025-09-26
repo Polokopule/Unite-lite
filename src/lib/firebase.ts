d95ec396d695fe6ab7031946c8b5b90b86b8c7d3
BD- SCHOOL <polokopule91@gmail.com>
2025-09-26 22:55:03 +0000

    How can I link my existing project with this workshop

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
@@ -5,7 +6,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "API_KEY",
+  apiKey: "YOUR_API_KEY",
   authDomain: "unite-3409c.firebaseapp.com",
   databaseURL: "https://unite-3409c-default-rtdb.firebaseio.com/",
   projectId: "unite-3409c",
@@ -20,3 +21,4 @@ const db = getDatabase(app);
 const auth = getAuth(app);
 
 export { db, auth, app };
+
