de5f4c57ff3aef9e3e312816dc35bdffe5b655f4
BD- SCHOOL <polokopule91@gmail.com>
2025-09-26 23:18:50 +0000

    TypeError: react_dom_1.default.findDOMNode is not a function     at Reac

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,16 +5,16 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyCYlzqp4ohFgH-Nw1NdIIkKr4U3HX7xwls",
   authDomain: "unite-3409c.firebaseapp.com",
-  databaseURL: "https://unite-3409c-default-rtdb.firebaseio.com/",
+  databaseURL: "https://unite-3409c-default-rtdb.firebaseio.com",
   projectId: "unite-3409c",
   storageBucket: "unite-3409c.appspot.com",
-  messagingSenderId: "MSID",
-  appId: "APP_ID"
+  messagingSenderId: "608397760209",
+  appId: "1:608397760209:web:96c7edde0c8d59e49b17f0"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
