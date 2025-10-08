abf03b601d1175d16b401a3a87f279e74bdb01f2
BD- SCHOOL <polokopule91@gmail.com>
2025-10-08 20:00:08 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2">
        <Image
-          src="https://storage.googleapis.com/stey-public/unite-logo-2.png"
+          src="https://raw.githubusercontent.com/Polokopule/UM/c75512e1eb1db9a842359e7e8d145755832a5d62/20250929_192455.png"
           alt="Unite Logo"
           width={28}
           height={28}
