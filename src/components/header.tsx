56589b8bffa7d9bb1b6fd0a33ca1d879af6996fd
BD- SCHOOL <polokopule91@gmail.com>
2025-10-09 23:39:38 +0000

    Remove my avatar from the nav bar instead replace it with well designed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -71,23 +71,27 @@ function UserSheet() {
     return (
         <Sheet open={open} onOpenChange={setOpen}>
             <SheetTrigger asChild>
-                 <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-                    <Avatar className="h-8 w-8">
-                        {user.photoURL && <AvatarImage src={user.photoURL} alt={user.name} />}
-                        <AvatarFallback className="bg-primary text-primary-foreground font-bold">
-                        {getInitials(user.name)}
-                        </AvatarFallback>
-                    </Avatar>
+                 <Button variant="ghost" size="icon">
+                    <Menu />
+                    <span className="sr-only">Open Menu</span>
                 </Button>
             </SheetTrigger>
             <SheetContent>
                 <SheetHeader>
                     <SheetTitle>
-                        <div className="flex flex-col space-y-1">
-                            <p className="text-sm font-medium leading-none">{user.name}</p>
-                            <p className="text-xs leading-none text-muted-foreground">
-                            {user.email}
-                            </p>
+                        <div className="flex items-center gap-3">
+                             <Avatar className="h-9 w-9">
+                                {user.photoURL && <AvatarImage src={user.photoURL} alt={user.name} />}
+                                <AvatarFallback className="bg-primary text-primary-foreground font-bold">
+                                {getInitials(user.name)}
+                                </AvatarFallback>
+                            </Avatar>
+                             <div className="flex flex-col space-y-1">
+                                <p className="text-sm font-medium leading-none">{user.name}</p>
+                                <p className="text-xs leading-none text-muted-foreground">
+                                {user.email}
+                                </p>
+                            </div>
                         </div>
                     </SheetTitle>
                 </SheetHeader>
@@ -149,7 +153,9 @@ export function Header() {
           </nav>
           <div className="flex items-center gap-4">
             {user ? (
-                <UserSheet />
+                <div className="md:hidden">
+                    <UserSheet />
+                </div>
             ) : (
               <>
                 <div className="hidden md:flex items-center gap-2">
