ebef4b22c838a7b8ec203bd29de3ed1a762ab2f9
BD-SCHOOL-OF-CREATION <96343216+Polokopule@users.noreply.github.com>
2025-10-18 23:29:33 +0200

    Update layout.tsx
    Ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+<meta name="google-adsense-account" content="ca-pub-6024568817887379">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6024568817887379"
+     crossorigin="anonymous"></script>
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
